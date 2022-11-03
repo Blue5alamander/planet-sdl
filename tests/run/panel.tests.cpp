204c33050d1e7e1fbc10a8d1c9b5e6c6b0c6f27e
Kirit Sælensminde <kirit@kirit.com>
2022-11-03 14:11:50 +0700

    Add another test that the hierarchy manipulation is correct

--- tests/run/panel.tests.cpp
+++ tests/run/panel.tests.cpp
@@ -8,10 +8,18 @@ namespace {
     auto const suite = felspar::testsuite("panel");
 
 
-    auto const d = suite.test("default", [](auto check) {
+    auto const h1 = suite.test("hierarchy 1", [](auto) {
         planet::sdl::panel p, pc;
         p.viewport.reflect_y();
         p.add_child(pc, {3, 4}, {5, 6});
+    });
+    auto const h2 = suite.test("hierarchy 2", [](auto) {
+        planet::sdl::panel p1, p3;
+        {
+            planet::sdl::panel p2;
+            p1.add_child(p2, {3, 4}, {5, 6});
+            p2.add_child(p3, {1, 2}, {8, 9});
+        }
     });
 
 
