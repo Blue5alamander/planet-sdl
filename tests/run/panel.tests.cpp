43e33a9c7f0cf737ef41823ffb44a58a947dbe93
Kirit Sælensminde <kirit@kirit.com>
2022-11-03 10:59:02 +0700

    Add test runner for panels.

---
+++ tests/run/panel.tests.cpp
@@ -0,0 +1,16 @@
+#include <planet/sdl/renderer.hpp>
+#include <felspar/test.hpp>
+
+
+namespace {
+
+
+    auto const suite = felspar::testsuite("panel");
+
+
+    auto const d = suite.test("default", [](auto) {
+        // planet::sdl::panel p;
+    });
+
+
+}
