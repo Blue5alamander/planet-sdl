6839a73edad2c9b2788065abbaa7f926d9a51918
Kirit Sælensminde <kirit@felspar.com>
2022-12-04 10:03:37 +0700

    Move UI code to planet

--- include/planet/sdl/ui.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include <planet/sdl/ui/button.hpp>
-#include <planet/sdl/ui/box.hpp>
-#include <planet/sdl/ui/layout.hpp>
--- include/planet/sdl/ui/layout.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-
-
-#include <planet/sdl/ui/layout.column.hpp>
-#include <planet/sdl/ui/layout.grid.hpp>
-#include <planet/sdl/ui/layout.row.hpp>
