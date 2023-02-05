0b9ffd27992e6a55fdece9ac04e554965f8e8728
Kirit Sælensminde <kirit@felspar.com>
2023-02-05 14:04:15 +0700

    Add some additional documentation

--- include/planet/sdl/render-loop.hpp
+++ include/planet/sdl/render-loop.hpp
@@ -7,10 +7,18 @@
 namespace planet::sdl {
 
 
-    /// Manages the instantiation and destruction of the render loop. Typically
-    /// this should be the last member in a structure so it destructed first as
-    /// the render loop implementation will typically involve access to a lot of
-    /// member data within its structure.
+    /// ## Render loop
+    /**
+     * Manages the instantiation and destruction of the render loop. It should
+     * be passed an object and member that implements the rendering loop stream
+     * and this will ensure that the render loop is properly stopped and
+     * destructed when the owning object of the `render_loop` instance is
+     * destructed.
+     *
+     * Typically this should be the last member in a structure so it destructed
+     * first as the render loop implementation will typically involve access to
+     * a lot of member data within its structure.
+     */
     struct render_loop final {
         template<typename N>
         render_loop(N &o, felspar::coro::stream<renderer::frame> (N::*f)()) {
@@ -18,10 +26,6 @@ namespace planet::sdl {
                     render_loop::frame_wrapper<N>, this, std::ref(o), f);
         }
 
-        /**
-         * When a render function has been connected (see `connect`) the
-         * awaitable returned here can be used to wait for the next frame.
-         */
         auto next_frame() { return waiting_for_frame.next(); }
 
       private:
