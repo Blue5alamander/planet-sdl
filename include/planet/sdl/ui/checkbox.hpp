b2cf76f280a53dc6ceb296d26afe5a113b22e462
Kirit Sælensminde <kirit@felspar.com>
2023-04-05 08:38:59 +0700

    Re-order some code and make attributes private

--- include/planet/sdl/ui/checkbox.hpp
+++ include/planet/sdl/ui/checkbox.hpp
@@ -10,21 +10,14 @@ namespace planet::sdl::ui {
 
 
     class checkbox {
-      public:
         planet::panel panel;
         texture on, off;
         bool &value;
         bool visible = false;
 
+      public:
         checkbox(renderer &r, surface on, surface off, bool &v)
         : on{r, std::move(on)}, off{r, std::move(off)}, value{v} {}
-
-        affine::extents2d extents(affine::extents2d const &ex) const {
-            auto const on_size = on.extents(ex);
-            auto const off_size = off.extents(ex);
-            return {std::max(on_size.width, off_size.width),
-                    std::max(on_size.height, off_size.height)};
-        }
 
         void add_to(planet::panel &parent) {
             parent.add_child(panel);
@@ -32,6 +25,12 @@ namespace planet::sdl::ui {
             visible = true;
         }
 
+        affine::extents2d extents(affine::extents2d const &ex) const {
+            auto const on_size = on.extents(ex);
+            auto const off_size = off.extents(ex);
+            return {std::max(on_size.width, off_size.width),
+                    std::max(on_size.height, off_size.height)};
+        }
         void draw_within(renderer &r, affine::rectangle2d const outer) {
             if (visible) {
                 if (value) {
