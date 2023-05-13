1b32d707fdad93c7567c6d414024523a4255c828
Kirit Sælensminde <kirit@felspar.com>
2023-05-13 12:00:01 +0700

    Move device independend button and checkbox widgets to planet library

--- include/planet/sdl/ui/button.hpp
+++ include/planet/sdl/ui/button.hpp
@@ -3,57 +3,41 @@
 
 #include <planet/sdl/surface.hpp>
 #include <planet/sdl/texture.hpp>
-#include <planet/ui/widget.hpp>
+#include <planet/ui/button.hpp>
 
 
 namespace planet::sdl::ui {
 
 
-    template<typename R>
-    class button : public planet::ui::widget<renderer> {
-        using superclass = planet::ui::widget<renderer>;
-        R press_value;
-        felspar::coro::bus<R> &output_to;
+    template<typename Return>
+    class button final : public planet::ui::button<renderer, Return, texture> {
+        using superclass = planet::ui::button<renderer, Return, texture>;
 
       public:
-        button(sdl::renderer &r, surface text, felspar::coro::bus<R> &o, R v)
-        : superclass{"planet::sdl::ui::button"},
-          press_value{std::move(v)},
-          output_to{o},
-          graphic{"planet::sdl::ui::button.graphic", r, std::move(text)} {}
+        using superclass::graphic;
+
+        button(sdl::renderer &r,
+               surface text,
+               felspar::coro::bus<Return> &o,
+               Return v)
+        : superclass{
+                "planet::sdl::ui::button",
+                {"planet::sdl::ui::button.graphic", r, std::move(text)},
+                o,
+                std::move(v)} {}
         button(std::string_view const n,
                sdl::renderer &r,
                surface text,
-               felspar::coro::bus<R> &o,
-               R v)
-        : superclass{n},
-          press_value{std::move(v)},
-          output_to{o},
-          graphic{std::string{n} + ".graphic", r, std::move(text)} {}
-
-        affine::extents2d extents(affine::extents2d const &ex) const {
-            return graphic.extents(ex);
-        }
-
-        texture graphic;
+               felspar::coro::bus<Return> &o,
+               Return v)
+        : superclass{
+                n,
+                {std::string{n} + ".graphic", r, std::move(text)},
+                o,
+                std::move(v)} {}
 
       private:
-        constrained_type do_reflow(constrained_type const &ex) override {
-            return graphic.reflow(ex);
-        }
-        void do_move_sub_elements(affine::rectangle2d const &r) override {
-            graphic.move_to(r);
-        }
-
         void do_draw(renderer &r) override { graphic.draw(r); }
-
-        felspar::coro::task<void> behaviour() override {
-            for (auto clicks = events::identify_clicks(
-                         baseplate->mouse_settings, events.mouse.stream());
-                 auto click = co_await clicks.next();) {
-                output_to.push(press_value);
-            }
-        }
     };
 
 
--- include/planet/sdl/ui/checkbox.hpp
+++ include/planet/sdl/ui/checkbox.hpp
@@ -3,72 +3,38 @@
 
 #include <planet/sdl/surface.hpp>
 #include <planet/sdl/texture.hpp>
-#include <planet/ui/widget.hpp>
+#include <planet/ui/checkbox.hpp>
 
 
 namespace planet::sdl::ui {
 
 
-    class checkbox final : public planet::ui::widget<renderer> {
-        using superclass = planet::ui::widget<renderer>;
-        texture on, off;
-        bool &value;
+    class checkbox final : public planet::ui::checkbox<renderer, texture> {
+        using superclass = planet::ui::checkbox<renderer, texture>;
 
       public:
         explicit checkbox(renderer &r, surface on, surface off, bool &v)
-        : superclass{"planet::sdl::ui::checkbox"},
-          on{r, std::move(on)},
-          off{r, std::move(off)},
-          value{v} {}
+        : superclass{
+                "planet::sdl::ui::checkbox",
+                {r, std::move(on)},
+                {r, std::move(off)},
+                {v}} {}
         explicit checkbox(
                 std::string_view const n,
                 renderer &r,
                 surface on,
                 surface off,
                 bool &v)
-        : superclass{n}, on{r, std::move(on)}, off{r, std::move(off)}, value{v} {}
+        : superclass{n, {r, std::move(on)}, {r, std::move(off)}, {v}} {}
 
-        using constrained_type = planet::ui::widget<renderer>::constrained_type;
-
-        affine::extents2d extents(affine::extents2d const &ex) const {
-            auto const on_size = on.extents(ex);
-            auto const off_size = off.extents(ex);
-            return {std::max(on_size.width, off_size.width),
-                    std::max(on_size.height, off_size.height)};
-        }
+        using constrained_type = superclass::constrained_type;
 
       private:
-        constrained_type do_reflow(constrained_type const &ex) override {
-            auto const on_size = on.reflow(ex);
-            auto const off_size = off.reflow(ex);
-            constrained_type::axis_constrained_type const w{
-                    std::max(on_size.width.min(), off_size.width.min()),
-                    std::max(on_size.width.value(), off_size.width.value()),
-                    std::min(on_size.width.max(), off_size.width.max())};
-            constrained_type::axis_constrained_type const h{
-                    std::max(on_size.height.min(), off_size.height.min()),
-                    std::max(on_size.height.value(), off_size.height.value()),
-                    std::min(on_size.height.max(), off_size.height.max())};
-            return {w, h};
-        }
-        void do_move_sub_elements(affine::rectangle2d const &r) override {
-            on.move_to(r);
-            off.move_to(r);
-        }
-
         void do_draw(renderer &r) override {
             if (value) {
                 on.draw(r);
             } else {
                 off.draw(r);
-            }
-        }
-
-        felspar::coro::task<void> behaviour() override {
-            for (auto clicks = events::identify_clicks(
-                         baseplate->mouse_settings, events.mouse.stream());
-                 co_await clicks.next();) {
-                value = not value;
             }
         }
     };
